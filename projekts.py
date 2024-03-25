bdaebc6d63576dd20d170f377a6e8b6f180e6978
anidiii <152158010+anidiii@users.noreply.github.com>
2024-03-25 10:19:43 +0200

    Add files via upload

---
+++ projekts.py
@@ -0,0 +1,8 @@
+from random import shuffle
+
+print(f"Sveiks, spēlētājs, laipni lūdzu tevi savā spēlē, uzmini vārdus!")
+print("Šīs spēles noteikumi ir vienkārši")
+print("Es izdomāju vārdu un saku, cik burtu tas satur")
+print("Jūs pārmaiņus zvanīsit pa vienai vēstulei")
+print("Ja burts ir manā vārdā, tad es tev to parādīšu")
+print("Jūs varat kļūdīties tik reižu, cik vienā vārdā ir burti, vienkārši reiziniet šo skaitli divas reizes")
